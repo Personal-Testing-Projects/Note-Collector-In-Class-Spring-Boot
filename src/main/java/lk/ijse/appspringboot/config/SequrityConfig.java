063a2c860f6d89e9582803593a66091392aa05a4
pathumck <ckpathumck@gmail.com>
2024-10-12 10:28:48 +0530

    Supply user credentials via value injection

--- src/main/java/lk/ijse/appspringboot/config/SequrityConfig.java
+++ src/main/java/lk/ijse/appspringboot/config/SequrityConfig.java
@@ -1,5 +1,6 @@
 package lk.ijse.appspringboot.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,13 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SequrityConfig {
+    @Value("${secure.username}")
+    private String userName;
+    @Value("${secure.password}")
+    private String password;
+    @Value("${secure.role}")
+    private String role;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
@@ -26,9 +34,9 @@ public class SequrityConfig {
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
         UserDetails principalUser = User.withDefaultPasswordEncoder()
-                .username("pathum")
-                .password("1234")
-                .roles("USER").build();
+                .username(userName)
+                .password(password)
+                .roles(role).build();
         return new InMemoryUserDetailsManager(principalUser);
     }
 }
