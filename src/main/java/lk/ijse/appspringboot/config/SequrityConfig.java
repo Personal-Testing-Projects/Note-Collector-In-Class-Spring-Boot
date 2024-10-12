c8f6aec0e2c5deb62c1fd3069ca533f0b50ea0b8
pathumck <ckpathumck@gmail.com>
2024-10-12 10:23:28 +0530

    Add spring security and test Basic Auth

---
+++ src/main/java/lk/ijse/appspringboot/config/SequrityConfig.java
@@ -0,0 +1,34 @@
+package lk.ijse.appspringboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SequrityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        //errors showing because they are deprecated methods
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails principalUser = User.withDefaultPasswordEncoder()
+                .username("pathum")
+                .password("1234")
+                .roles("USER").build();
+        return new InMemoryUserDetailsManager(principalUser);
+    }
+}
